5f6c9ce52187f9e6076299d0dae0e030f6aa41d0
Dagime Teshome <dagimeteshome1@gmail.com>
2026-08-19 20:01:40 +0300

    add file info agent function

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+
+
+import os
+
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+
+    try:
+        abs_wd_path = os.path.abspath(working_directory)
+        abs_tr_path = os.path.normpath(os.path.join(abs_wd_path,directory))
+        valid_tr_path = os.path.commonpath([abs_wd_path,abs_tr_path]) == abs_wd_path
+        if not os.path.isdir(abs_tr_path):
+            return f'Error: "{directory}" is not a directory'
+        if not valid_tr_path:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+        dir_contents = os.listdir(abs_tr_path)
+        return_txt = ""
+        dir = directory
+
+        return_txt += f'Result for {'current' if dir == "." else "'" + dir +"'"} directory:\n'
+        for dir in dir_contents:
+            return_txt += f'- {dir}: file_size={os.path.getsize(os.path.join(abs_tr_path,dir))} bytes, is_dir={os.path.isdir(os.path.join(abs_tr_path,dir))}\n'
+        return return_txt
+        # return f'Success: "{directory}" is within the working directory'
+    except Exception as e:
+        return f'Error:{e}'
