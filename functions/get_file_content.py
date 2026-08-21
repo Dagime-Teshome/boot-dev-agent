232363cf3cbb085d3af5dc890c7c9da5d42db9ac
Dagime Teshome <dagimeteshome1@gmail.com>
2026-08-21 15:54:15 +0300

    add a file content function
    -read content of file
    -used by agent

---
+++ functions/get_file_content.py
@@ -0,0 +1,19 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+           abs_wd_path = os.path.abspath(working_directory)
+           abs_tr_path = os.path.normpath(os.path.join(abs_wd_path,file_path))
+           valid_tr_path = os.path.commonpath([abs_wd_path,abs_tr_path]) == abs_wd_path
+           if not os.path.isfile(abs_tr_path):
+               return f'Error: File not found or is not a regular file: "{file_path}"'
+           if not valid_tr_path:
+               return f'Error: Cannot list "{file_path}" as it is outside the permitted working file_path'
+           with open(abs_tr_path) as f:
+                content = f.read(MAX_CHARS)
+                if f.read(1):
+                    content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+           return content
+    except Exception as e:
+        return f'Error:{e}'
